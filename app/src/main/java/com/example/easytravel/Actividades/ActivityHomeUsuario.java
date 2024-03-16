67e71e2c7bab36c2ab5f98307f504d0912781808
Carlos de León <54327606+juancaco10@users.noreply.github.com>
2024-03-15 23:21:00 -0300

    update

--- app/src/main/java/com/example/easytravel/Actividades/ActivityHomeUsuario.java
+++ app/src/main/java/com/example/easytravel/Actividades/ActivityHomeUsuario.java
@@ -11,6 +11,6 @@ public class ActivityHomeUsuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nueva);
+        setContentView(R.layout.activity_home_usuario);
     }
 }
--- app/src/main/java/com/example/easytravel/Actividades/ActivityLoginUsuario.java
+++ app/src/main/java/com/example/easytravel/Actividades/ActivityLoginUsuario.java
@@ -30,7 +30,7 @@ public class ActivityLoginUsuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.activity_login_usuario);
 
         // Inicializar FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
