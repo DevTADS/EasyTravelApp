1e93d59424f7424bef97b950a904b2422d8a8681
GualbertoCasas <cgualberto53@gmail.com>
2024-03-17 00:00:43 -0300

    gualberto actulizar1

--- app/src/main/java/com/example/easytravel/Actividades/ActivityLoginEmpresa.java
+++ app/src/main/java/com/example/easytravel/Actividades/ActivityLoginEmpresa.java
@@ -11,7 +11,7 @@ public class ActivityLoginEmpresa extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_empresa_homeeeeeeeee);
+        setContentView(R.layout.activity_home_empresa);
 
 
     }
