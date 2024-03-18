0e95dbc40e5d7aa0e371aa569989bf5c6faa7afe
Carlos de León <54327606+juancaco10@users.noreply.github.com>
2024-03-18 12:12:25 -0300

    12:12 carlos

--- app/src/main/java/com/example/easytravel/Actividades/Usuario/ActivityHomeUsuario.java
+++ app/src/main/java/com/example/easytravel/Actividades/Usuario/ActivityHomeUsuario.java
@@ -17,17 +17,6 @@ public class ActivityHomeUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario_home);
 
-        // Obtener referencia al CardView
-        CardView cardViewHotel = findViewById(R.id.cardViewHotel);
 
-        // Agregar OnClickListener al CardView
-        cardViewHotel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Abrir la actividad de lista de hoteles
-                Intent intent = new Intent(ActivityHomeUsuario.this, ActivityListaHoteles.class);
-                startActivity(intent);
-            }
-        });
     }
 }
