ee9f1cf67b2cfcf4c0b390310e546293583ea4d8
juancaco10 <caco_deleon@hotmail.com>
2024-03-28 20:45:01 -0300

    Ultima actualizacion 08:00

--- app/src/main/java/com/example/easytravel/PersonaActivity.java
+++ app/src/main/java/com/example/easytravel/PersonaActivity.java
@@ -1,12 +1,10 @@
 package com.example.easytravel;
 
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,63 +19,55 @@ import retrofit2.Response;
 
 public class PersonaActivity extends AppCompatActivity {
     PersonaService service;
+    EditText txtNombres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.persona_layout);
 
-
-
-
-        TextView nombres = (TextView) findViewById(R.id.nombres);
-        final EditText txtNombres = (EditText) findViewById(R.id.txtNombres);
-
-
-
-
-        Button btnSave = (Button) findViewById(R.id.btnSave);
-
-
-
-
-
-
+        txtNombres = findViewById(R.id.txtNombres);
+        Button btnSave = findViewById(R.id.btnSave);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Persona p = new Persona();
-                p.setNombres(txtNombres.getText().toString());
-
-
-                addPersona(p);
-
-
+                agregarPersona();
             }
         });
-
-
     }
 
-    public void addPersona(Persona p) {
+    private void agregarPersona() {
+        Persona p = new Persona();
+        p.setNombres(txtNombres.getText().toString());
+
         service = Apis.getPersonaService();
         Call<Persona> call = service.addPersona(p);
         call.enqueue(new Callback<Persona>() {
             @Override
             public void onResponse(Call<Persona> call, Response<Persona> response) {
                 if (response.isSuccessful()) {
-                    Toast.makeText(PersonaActivity.this, "Se agrego conéxito", Toast.LENGTH_LONG).show();
+                    Toast.makeText(PersonaActivity.this, "Se agregó con éxito", Toast.LENGTH_LONG).show();
+                    limpiarCampos();
+                    volverActividadAnterior();
+                } else {
+                    Toast.makeText(PersonaActivity.this, "Error al agregar persona", Toast.LENGTH_LONG).show();
                 }
             }
 
             @Override
             public void onFailure(Call<Persona> call, Throwable t) {
                 Log.e("Error:", t.getMessage());
+                Toast.makeText(PersonaActivity.this, "Error al agregar persona", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 
+    private void limpiarCampos() {
+        txtNombres.setText("");
+    }
 
+    private void volverActividadAnterior() {
+        finish();
+    }
 }
--- app/src/main/java/com/example/easytravel/Utils/Apis.java
+++ app/src/main/java/com/example/easytravel/Utils/Apis.java
@@ -2,7 +2,7 @@ package com.example.easytravel.Utils;
 
 public class Apis {
 
-    public static final String URL_001="database-2.c9qw4koielmq.us-east-2.rds.amazonaws.com:8080/personas/";
+    public static final String URL_001 = "http://192.168.31.201:8080/personas/";
 
     public static PersonaService getPersonaService(){
         return  Cliente.getClient(URL_001).create(PersonaService.class);
