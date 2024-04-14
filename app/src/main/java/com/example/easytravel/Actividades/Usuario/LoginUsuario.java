5b3e9d15c7c074ebc3ca05876d85ad99eb094b50
juancaco10 <caco_deleon@hotmail.com>
2024-04-14 12:25:44 -0300

    actualizacion 12:25 horas - 14/04

--- app/src/main/java/com/example/easytravel/Actividades/Usuario/LoginUsuario.java
+++ app/src/main/java/com/example/easytravel/Actividades/Usuario/LoginUsuario.java
@@ -1,79 +1,107 @@
 package com.example.easytravel.Actividades.Usuario;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.MotionEvent;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.easytravel.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class LoginUsuario extends AppCompatActivity {
 
-    private EditText emailEditText;
-    private EditText passwordEditText;
-    private Button loginButton;
-
-    private Button registerButton;
-    private boolean passwordVisible = false;
+    EditText email, contraseña;
+    String str_email, str_password;
+    String url = "https://tejuqiaq.lucusvirtual.es/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_usuario_login);
 
+        email = findViewById(R.id.etemail);
+        contraseña = findViewById(R.id.etcontraseña);
 
-
-        // Inicializar vistas
-        emailEditText = findViewById(R.id.username);
-        passwordEditText = findViewById(R.id.password);
-        loginButton = findViewById(R.id.btn_login);
-        registerButton = findViewById(R.id.btn_registro);
-
-
-
-        // Configurando OnClickListener para el botón de registro
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        Button btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Abrir la actividad de registro al hacer clic en el botón de registro
-                startActivity(new Intent(LoginUsuario.this, RegistroUsuario.class));
+                Login();
             }
         });
 
-        // Configurando OnClickListener para el ícono de la contraseña
-        passwordEditText.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.icono_password, 0);
-        passwordEditText.setOnTouchListener(new View.OnTouchListener() {
+        Button btnRegister = findViewById(R.id.btn_register);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_RIGHT = 2;
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (passwordEditText.getRight() - passwordEditText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-                        togglePasswordVisibility();
-                        return true;
-                    }
-                }
-                return false;
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginUsuario.this, RegistroUsuario.class);
+                startActivity(intent);
             }
         });
     }
 
-    private void togglePasswordVisibility() {
-        if (passwordVisible) {
-            // Cambiar a contraseña oculta
-            passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-            passwordVisible = false;
+    private void Login() {
+        if (email.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+        } else if (contraseña.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
         } else {
-            // Cambiar a texto visible
-            passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
-            passwordVisible = true;
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Por favor espera...");
+            progressDialog.show();
+
+            str_email = email.getText().toString().trim();
+            str_password = contraseña.getText().toString().trim();
+
+            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+                    if (response.equalsIgnoreCase("ingreso correctamente")) {
+                        email.setText("");
+                        contraseña.setText("");
+                        startActivity(new Intent(getApplicationContext(), HomeUsuario.class));
+                        Toast.makeText(LoginUsuario.this, response, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginUsuario.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginUsuario.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("email", str_email);
+                    params.put("password", str_password);
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(LoginUsuario.this);
+            requestQueue.add(request);
         }
-        // Mover el cursor al final del texto
-        passwordEditText.setSelection(passwordEditText.getText().length());
     }
 }
--- app/src/main/java/com/example/easytravel/Main/MainActivity.java
+++ app/src/main/java/com/example/easytravel/Main/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.easytravel.Main;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.easytravel.Model.Usuario;
 import com.example.easytravel.R;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn_identificacion = findViewById(R.id.btn_identificacion);
         btn_identificacion.setOnClickListener(v -> {
-            // Intent para iniciar la actividad ActivityInicioSesion
+
             Intent intent = new Intent(MainActivity.this, ActivityInicioSesion.class);
             startActivity(intent);
 
 
         });
     }
-
-    // Método para verificar la conexión con el servidor
-
 }
