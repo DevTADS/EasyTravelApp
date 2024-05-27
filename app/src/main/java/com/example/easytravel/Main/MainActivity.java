b737af59a01be6ea2a7e86be2f9fb4566d955789
juancaco10 <caco_deleon@hotmail.com>
2024-05-26 22:50:22 -0300

    ultima 22:50  26/05

--- app/src/main/java/com/example/easytravel/Main/MainActivity.java
+++ app/src/main/java/com/example/easytravel/Main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.easytravel.Main;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,19 +9,29 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.easytravel.Actividades.Empresa.LoginEmpresa;
+import com.example.easytravel.Actividades.Usuario.HomeeUsuario;
 import com.example.easytravel.Actividades.Usuario.LoginUsuario;
 import com.example.easytravel.R;
 
-
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Verificar si el usuario ha iniciado sesión
+        SharedPreferences sharedPreferences = getSharedPreferences("Usuario", MODE_PRIVATE);
+        if (sharedPreferences.contains("id_usuario")) {
+            // El usuario ya ha iniciado sesión, redirigir a HomeUsuario
+            Intent intent = new Intent(MainActivity.this, HomeeUsuario.class);
+            startActivity(intent);
+            finish();
+            return; // Salir del método onCreate para no cargar el layout de selección de tipo de usuario
+        }
+
         setContentView(R.layout.activity_main);
 
-
+        // Botón para ingresar como usuario
         Button btn_usuario = findViewById(R.id.btn_usuario);
         btn_usuario.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Botón para ingresar como empresa
         Button btn_empresa = findViewById(R.id.btn_empresa);
         btn_empresa.setOnClickListener(new View.OnClickListener() {
             @Override
